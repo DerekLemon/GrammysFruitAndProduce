2e134953e4bdbe4510def498a4c87722a4b69d13
Derek Lemon <dklemo1@gmail.com>
2016-11-15 18:51:27 -0700

    add project

---
+++ GrammysFruitAndProduce/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GrammysFruitAndProduce.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ GrammysFruitAndProduce/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GrammysFruitAndProduce.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
